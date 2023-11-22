e12c3843d8216c31a2b3a1f953e2d92c580bb5d0
Rodrigo <22302066@live.uem.es>
2023-11-22 11:09:29 +0100

    hay que eliminar este archivop

--- data/datos.sql
+++ data/datos.sql
@@ -1,27 +1,34 @@
--- Insertar pacientes
-INSERT INTO paciente (nombre, informacion_medica) VALUES
-    ('Laura Gómez', 'Historial médico: Problemas cardíacos'),
-    ('Carlos Pérez', 'Historial médico: Alergia a la piel'),
-    ('Sofía García', 'Historial médico: Migrañas frecuentes'),
-    ('Pedro Rodríguez', 'Historial médico: Vacunación infantil');
-
 -- Insertar médicos
-INSERT INTO medico (nombre, especialidad) VALUES
-    ('Dr. Martínez', 'Cardiología'),
-    ('Dra. Sánchez', 'Dermatología'),
-    ('Dr. Pérez', 'Neurología'),
-    ('Dra. Rodríguez', 'Pediatría');
+INSERT INTO medico (nombre, apellidos, especialidad) VALUES
+    ('Dr. Martínez', 'Gómez', 'Cardiología'),
+    ('Dra. Sánchez', 'Pérez', 'Dermatología'),
+    ('Dr. Pérez', 'García', 'Neurología'),
+    ('Dra. Rodríguez', 'Rodríguez', 'Pediatría');
 
--- Insertar citas
-INSERT INTO cita (id_paciente, id_medico, fecha) VALUES
-    (1, 1, '2023-12-01 10:00:00'),
-    (2, 2, '2023-12-02 11:30:00'),
-    (3, 3, '2023-12-03 09:45:00'),
-    (4, 4, '2023-12-04 15:15:00');
+-- Insertar pacientes
+INSERT INTO paciente (dni, nombre, apellidos, genero, fecha_nac, id_medicos_asignados) VALUES
+    ('123456789', 'Laura', 'Gómez', 'F', '1990-05-15', '1'),
+    ('987654321', 'Carlos', 'Pérez', 'M', '1985-08-22', '2'),
+    ('456789123', 'Sofía', 'García', 'F', '2000-12-10', '3'),
+    ('789123456', 'Pedro', 'Rodríguez', 'M', '1998-02-28', '4');
 
 -- Insertar consultas
-INSERT INTO consulta (id_cita, sintomatologia, diagnostico, pdf, fecha_registro) VALUES
-    (1, 'Dolor en el pecho', 'Necesita más pruebas', 'adjunto.pdf', '2023-12-01 11:30:00'),
-    (2, 'Erupción en la piel', 'Alergia leve', NULL, '2023-12-02 13:00:00'),
-    (3, 'Dolores de cabeza frecuentes', 'Migrañas', 'informe.pdf', '2023-12-03 11:00:00'),
-    (4, 'Vacunación infantil', 'En buen estado de salud', NULL, '2023-12-04 16:00:00');
+INSERT INTO consulta (id_medico, id_paciente, fecha_consulta, diagnostico, sintomatologia) VALUES
+    (1, 1, '2023-12-01', 'Pruebas adicionales necesarias', 'Dolor en el pecho'),
+    (2, 2, '2023-12-02', 'Alergia leve', 'Erupción en la piel'),
+    (3, 3, '2023-12-03', 'Migrañas', 'Dolores de cabeza frecuentes'),
+    (4, 4, '2023-12-04', 'En buen estado de salud', 'Vacunación infantil');
+
+-- Insertar medicamentos
+INSERT INTO medicamento (nombre) VALUES
+    ('Paracetamol'),
+    ('Ibuprofeno'),
+    ('Amoxicilina'),
+    ('Loratadina');
+
+-- Insertar recetas
+INSERT INTO receta (id_medicamento, id_consulta, posologia, fecha_fin) VALUES
+    (1, 1, 'Tomar cada 8 horas', '2023-12-10'),
+    (2, 2, 'Tomar cada 12 horas', '2023-12-15'),
+    (3, 3, 'Tomar una vez al día', '2023-12-20'),
+    (4, 4, 'Tomar según necesidad', NULL);
