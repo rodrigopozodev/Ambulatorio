45be9801a8997d0f11c3956e4862ad6469500bf9
Rodrigo <22302066@live.uem.es>
2023-11-22 09:22:18 +0100

    Archivo setup.php corregido

--- scripts/setup.php
+++ scripts/setup.php
@@ -1,30 +1,25 @@
 <?php
-$conexion = new mysqli('localhost', 'root', '', 'Ambulatorio');
+$conexion = new mysqli('localhost', 'root', '', '');
 
 // Verificar la conexión
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
 }
 
-// Verificar si la base de datos existe
-$resultado = $conexion->query("SHOW DATABASES LIKE 'Ambulatorio'");
+// Crear la base de datos
+$crearBD = $conexion->query("CREATE DATABASE IF NOT EXISTS Ambulatorio");
 
-// Si no existe, la creamos
-if ($resultado->num_rows === 0) {
-    $crearBD = $conexion->query("CREATE DATABASE Ambulatorio");
-
-    if (!$crearBD) {
-        die("Error al crear la base de datos: " . $conexion->error);
-    }
-
-    // Luego, seleccionamos la base de datos recién creada
-    $conexion->select_db("Ambulatorio");
-
-    // Aquí puedes incluir la lógica para crear tablas e insertar datos
-    // ...
-
-    echo "Base de datos Ambulatorio creada con éxito.";
+if (!$crearBD) {
+    die("Error al crear la base de datos: " . $conexion->error);
 }
+
+// Luego, seleccionamos la base de datos recién creada
+$conexion->select_db("Ambulatorio");
+
+// Aquí puedes incluir la lógica para crear tablas e insertar datos
+// ...
+
+echo "Base de datos Ambulatorio creada con éxito.";
 
 // Cerrar la conexión
 $conexion->close();
