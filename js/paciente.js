aea119444ea7ced94a157bcc5fa1825278a4e1ee
Rodrigo <22302066@live.uem.es>
2023-11-29 11:13:58 +0100

    Actualizaciones varias

--- js/paciente.js
+++ js/paciente.js
@@ -1,5 +1,3 @@
-// Archivo: js/paciente.js
-
 // Función para cargar información del paciente desde el servidor
 function cargarInfoPaciente(idPaciente) {
     // Realizar una solicitud al servidor para obtener la información del paciente
@@ -52,3 +50,40 @@ function solicitarCita(idPaciente, idMedico, fecha, sintomas) {
         })
         .catch(error => console.error('Error al solicitar cita:', error));
 }
+
+// Función para cargar la lista de pacientes
+function cargarListaPacientes() {
+    // Realizar una solicitud al servidor para obtener la lista de pacientes
+    // Puedes usar Fetch API, XMLHttpRequest u otra biblioteca para hacer la solicitud
+    // Aquí se proporciona un ejemplo simple usando Fetch API
+    fetch('servidor.php?tipo=listaPacientes')
+        .then(response => response.json())
+        .then(data => {
+            // Actualizar la interfaz con la lista de pacientes
+            const selectPaciente = document.getElementById('select-paciente');
+
+            // Limpiar opciones existentes
+            selectPaciente.innerHTML = '';
+
+            // Agregar opciones para cada paciente
+            data.forEach(paciente => {
+                const option = document.createElement('option');
+                option.value = paciente.id;
+                option.textContent = `${paciente.nombre} ${paciente.apellidos}`;
+                selectPaciente.appendChild(option);
+            });
+
+            // Llamar a la función para cargar información del paciente seleccionado
+            cargarInfoPaciente(selectPaciente.value);
+
+            // Agregar evento para cambiar la información del paciente cuando se selecciona uno diferente
+            selectPaciente.addEventListener('change', function() {
+                cargarInfoPaciente(this.value);
+                // También puedes llamar a otras funciones según sea necesario
+            });
+        })
+        .catch(error => console.error('Error al cargar la lista de pacientes:', error));
+}
+
+// Llamada a la función para cargar la lista de pacientes al cargar la página
+cargarListaPacientes();
