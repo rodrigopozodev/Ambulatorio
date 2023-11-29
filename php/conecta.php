6558e0bdaddfea582d865f76088081b251e6db22
Rodrigo <22302066@live.uem.es>
2023-11-29 09:21:49 +0100

    Conecta.php y crea_tablas.php finalizados

--- php/conecta.php
+++ php/conecta.php
@@ -1,16 +1,43 @@
 <?php
+
 function getConexion() {
     $servidor = "localhost";
     $usuario = "root";
     $contrasena = "";
     $bd = "Ambulatorio";
 
+    // Crear una conexión
     $conexion = new mysqli($servidor, $usuario, $contrasena);
 
+    // Verificar la conexión
     if ($conexion->connect_error) {
         die("Error de conexión: " . $conexion->connect_error);
     }
 
+    // Verificar si la base de datos existe
+    $resultado = $conexion->query("SHOW DATABASES LIKE 'Ambulatorio'");
+
+    // Si no existe, la creamos
+    if ($resultado->num_rows === 0) {
+        $crearBD = $conexion->query("CREATE DATABASE Ambulatorio");
+
+        if (!$crearBD) {
+            die("Error al crear la base de datos: " . $conexion->error);
+        }
+
+        // Luego, seleccionamos la base de datos recién creada
+        $conexion->select_db("Ambulatorio");
+
+        echo "Base de datos Ambulatorio creada con éxito.";
+    }
+
     return $conexion;
 }
+
+// Obtener la conexión
+$conexion = getConexion();
+
+// Cerrar la conexión al finalizar el script
+$conexion->close();
+
 ?>
