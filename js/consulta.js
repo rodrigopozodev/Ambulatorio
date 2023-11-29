fb27e3eac8baf841ca5c3b150801626392bc7107
Rodrigo <22302066@live.uem.es>
2023-11-29 10:27:44 +0100

    3º versión

--- js/consulta.js
+++ js/consulta.js
@@ -1,61 +1,68 @@
-// Archivo: js/consulta.js
+// Lógica para añadir medicación a la lista y actualizar la interfaz
+function anadirMedicacionALaLista(medicacion) {
+    // Crear un elemento de lista para mostrar la medicación
+    const listaMedicacion = document.getElementById("listaMedicacion");
+    const nuevoElemento = document.createElement("li");
 
-// Función para cargar la información de la consulta desde el servidor
-function cargarConsultaInfo(idConsulta) {
-    // Realizar una solicitud al servidor para obtener la información de la consulta
-    // Puedes usar Fetch API, XMLHttpRequest u otra biblioteca para hacer la solicitud
-    // Aquí se proporciona un ejemplo simple usando Fetch API
-    fetch('servidor.php?idConsulta=' + idConsulta)
-        .then(response => response.json())
-        .then(data => {
-            // Actualizar la interfaz con la información de la consulta
-            console.log("Información de la consulta cargada:", data);
-        })
-        .catch(error => console.error('Error al cargar la información de la consulta:', error));
+    // Crear una cadena con la información de la medicación
+    let infoMedicacion = `${medicacion.medicamento} - Cantidad: ${medicacion.cantidad}, Frecuencia: ${medicacion.frecuencia}`;
+    if (medicacion.esCronica) {
+        infoMedicacion += ", Medicación crónica";
+    } else {
+        infoMedicacion += `, ${medicacion.numDias} días`;
+    }
+
+    // Asignar la cadena al nuevo elemento de lista
+    nuevoElemento.textContent = infoMedicacion;
+
+    // Añadir el nuevo elemento a la lista
+    listaMedicacion.appendChild(nuevoElemento);
+
+    // Limpiar los campos después de añadir la medicación
+    limpiarCamposMedicacion();
 }
 
-// Función para agregar medicación a la consulta
-function agregarMedicacion(idConsulta, medicacion) {
-    // Realizar una solicitud al servidor para agregar medicación a la consulta
-    // Puedes usar Fetch API, XMLHttpRequest u otra biblioteca para hacer la solicitud
-    // Aquí se proporciona un ejemplo simple usando Fetch API
-    fetch('servidor.php', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            idConsulta: idConsulta,
-            medicacion: medicacion,
-        }),
-    })
-        .then(response => response.json())
-        .then(data => {
-            // Manejar la respuesta del servidor
-            console.log("Respuesta del servidor:", data);
-        })
-        .catch(error => console.error('Error al agregar medicación:', error));
+// Función para derivar a un especialista y actualizar la interfaz
+function derivarAEspecialista(idEspecialista, fechaCitaEspecialista) {
+    // Simulación de la solicitud al servidor para registrar la derivación
+    const solicitudExitosa = simularSolicitudAlServidor(idEspecialista, fechaCitaEspecialista);
+
+    // Verificar si la solicitud fue exitosa
+    if (solicitudExitosa) {
+        // Actualizar la interfaz para reflejar la derivación
+        mostrarMensaje("Derivación exitosa. Se ha programado una cita con el especialista.");
+        limpiarCamposDerivacion();
+    } else {
+        // Mostrar un mensaje de error si la solicitud falla
+        mostrarMensaje("Error al derivar a un especialista. Por favor, inténtelo de nuevo.");
+    }
 }
 
-// Función para registrar la consulta en el servidor
-function registrarConsulta(idConsulta, diagnostico) {
-    // Realizar una solicitud al servidor para registrar la consulta
-    // Puedes usar Fetch API, XMLHttpRequest u otra biblioteca para hacer la solicitud
-    // Aquí se proporciona un ejemplo simple usando Fetch API
-    fetch('servidor.php', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            idConsulta: idConsulta,
-            diagnostico: diagnostico,
-        }),
-    })
-        .then(response => response.json())
-        .then(data => {
-            // Manejar la respuesta del servidor
-            console.log("Respuesta del servidor:", data);
-        })
-        .catch(error => console.error('Error al registrar la consulta:', error));
+// Función para simular una solicitud al servidor
+function simularSolicitudAlServidor(idEspecialista, fechaCitaEspecialista) {
+    // Lógica para simular la solicitud al servidor
+    // Aquí puedes realizar una solicitud AJAX o usar fetch para enviar datos al servidor
+
+    // Simulamos que la solicitud siempre es exitosa
+    return true;
+}
+
+// Función para mostrar mensajes en la interfaz
+function mostrarMensaje(mensaje) {
+    // Mostrar el mensaje donde prefieras en tu interfaz
+    alert(mensaje); // Este es solo un ejemplo, puedes implementar tu propio sistema de mensajes
+}
+
+// Función para limpiar los campos de derivación después de realizar la acción
+function limpiarCamposDerivacion() {
+    // Puedes limpiar campos, reiniciar formularios, etc., según tus necesidades
+}
+
+// Función para limpiar los campos de medicación después de añadir a la lista
+function limpiarCamposMedicacion() {
+    document.getElementById("medicamento").value = "";
+    document.getElementById("cantidad").value = "";
+    document.getElementById("frecuencia").value = "";
+    document.getElementById("numDias").value = "";
+    document.getElementById("esCronica").checked = false;
 }
--- php/funciones.php
+++ php/funciones.php
@@ -1,78 +1,25 @@
 <?php
+
 include 'conecta.php';
 
-function obtenerMedicoPorId($id) {
-    $conexion = getConexion();
-    $query = "SELECT * FROM medico WHERE id_medico = $id";
-    $resultado = $conexion->query($query);
-    $medico = $resultado->fetch_assoc();
-    $conexion->close();
-    return $medico;
-}
+$idConsulta = $_GET['idConsulta'];
+error_log("ID de la consulta recibido: $idConsulta");
 
-function obtenerConsultasProximasMedico($idMedico) {
-    $conexion = getConexion();
-    $fechaHoy = date("Y-m-d");
-    $query = "SELECT * FROM consulta WHERE id_medico = $idMedico AND fecha_consulta >= '$fechaHoy'";
-    $resultado = $conexion->query($query);
-    $consultas = [];
-    while ($consulta = $resultado->fetch_assoc()) {
-        $consultas[] = $consulta;
-    }
-    $conexion->close();
-    return $consultas;
-}
-
-function cargarConsultaInfo($idConsulta) {
+function obtenerDatosConsulta($idConsulta) {
     $conexion = getConexion();
     $query = "SELECT * FROM consulta WHERE id_consulta = $idConsulta";
     $resultado = $conexion->query($query);
-    $consulta = $resultado->fetch_assoc();
+
+    if ($resultado->num_rows > 0) {
+        $consulta = $resultado->fetch_assoc();
+        return json_encode($consulta);
+    } else {
+        return json_encode(["error" => "Consulta no encontrada"]);
+    }
+
     $conexion->close();
-    return $consulta;
 }
 
-function agregarMedicacion($idConsulta, $medicacion) {
-    // Lógica para agregar medicación a la consulta en la base de datos
-    // Aquí puedes implementar la lógica según tus necesidades
-    return ['mensaje' => 'Medicación agregada con éxito'];
-}
+// Otras funciones según sea necesario (insertar, actualizar, etc.)
 
-function registrarConsulta($idConsulta, $diagnostico) {
-    // Lógica para registrar la consulta en la base de datos
-    // Aquí puedes implementar la lógica según tus necesidades
-    return ['mensaje' => 'Consulta registrada con éxito'];
-}
-
-// Manejar las solicitudes desde los archivos JavaScript
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    if (isset($_GET['idMedico'])) {
-        // Solicitud para obtener información del médico
-        $idMedico = $_GET['idMedico'];
-        $infoMedico = obtenerMedicoPorId($idMedico);
-        echo json_encode($infoMedico);
-    } elseif (isset($_GET['idConsulta'])) {
-        // Solicitud para cargar información de la consulta
-        $idConsulta = $_GET['idConsulta'];
-        $infoConsulta = cargarConsultaInfo($idConsulta);
-        echo json_encode($infoConsulta);
-    }
-} elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Manejar solicitudes POST
-    $tipoSolicitud = $_POST['tipo'] ?? '';
-
-    if ($tipoSolicitud === 'agregarMedicacion') {
-        // Solicitud para agregar medicación
-        $idConsulta = $_POST['idConsulta'];
-        $medicacion = $_POST['medicacion'];
-        $respuesta = agregarMedicacion($idConsulta, $medicacion);
-        echo json_encode($respuesta);
-    } elseif ($tipoSolicitud === 'registrarConsulta') {
-        // Solicitud para registrar la consulta
-        $idConsulta = $_POST['idConsulta'];
-        $diagnostico = $_POST['diagnostico'];
-        $respuesta = registrarConsulta($idConsulta, $diagnostico);
-        echo json_encode($respuesta);
-    }
-}
 ?>
